d242a53ff7a27ce188324f0aefbd2f92d2835505
Dhusyanth2005 <sdhusyanth20@gmail.com>
2025-03-04 00:51:25 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
@@ -5,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import { v2 as cloudinary } from "cloudinary";
-import cors from 'cors';
+
 
 
 dotenv.config();
@@ -14,11 +15,7 @@ connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-}));
+const __dirname = path.resolve();
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -32,6 +29,17 @@ app.use(cookieParser());
 
 app.use('/api/users',userRoutes)
 app.use('/api/post',postRoutes)
+
+// http://localhost:5000 => backend,frontend
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(port, () => {
     console.log('server is running on port', port);
